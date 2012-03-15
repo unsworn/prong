1998d4c225869b5d60e80f94f41a61fe3cefd12b
nim <nicklas@unsworn.org>
2012-03-15 17:38:25 +0100

    a

--- src/deskew/ytree.c
+++ src/deskew/ytree.c
@@ -465,7 +465,7 @@ yajl_val yajl_tree_parse (const char *input,
     status = yajl_parse(handle,
                         (unsigned char *) input,
                         strlen (input));
-    status = yajl_complete_parse (handle);
+    status = yajl_parse_complete (handle);
     if (status != yajl_status_ok) {
         if (error_buffer != NULL && error_buffer_size > 0) {
             snprintf(
